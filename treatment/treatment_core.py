89f8c8f114bcca941358d34d915596bd5027ed10
JuneBridge03 <junebridge0903@gmail.com>
2019-08-11 00:11:24 +0900

    #2 Updated Treatment Codes

--- treatment/treatment_core.py
+++ treatment/treatment_core.py
@@ -1,6 +1,10 @@
-import json
 import urllib.request
+import json
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import sak_core
 
 
 # id_treatment(id, name, version, document, images( id : image data //Not now))
@@ -10,12 +14,13 @@ def update_treatments():
     data = json.loads(
         urllib.request.urlopen(
             "http://sakproject.ml/treatments_list.json").read().decode())  # data ( id : integer , version : integer)
-    if not os.path.isdir("../resources/treatments"):
-        os.mkdir("../resources/treatments")
+    if not os.path.isdir(sak_core.RESOURCE_DIR_PATH + "treatments"):
+        os.mkdir(sak_core.RESOURCE_DIR_PATH + "treatments")
     for i in range(len(data)):
-        if not os.path.isfile("../resources/treatments/" + data[i]["id"] + "_treatment.sak"):
+        if not os.path.isfile(sak_core.RESOURCE_DIR_PATH + "treatments/" + data[i]["id"] + "_treatment.sak"):
             f = open(i + "_treatment.sak", "w+")
-            f.write(urllib.request.urlopen("http://sakproject.ml/" + i + "_treatments.sak").read().decode())
+            f.write(urllib.request.urlopen("http://sakproject.ml/" +
+                                           i + "_treatments.sak").read().decode())
             f.close()
         else:
             f = open(i + "_treatment.sak", "r+")
@@ -23,14 +28,15 @@ def update_treatments():
             if int(d["version"]) < data[i]["version"]:
                 f.close()
                 f = open(i + "_treatment.sak", "w+")
-                f.write(urllib.request.urlopen("http://sakproject.ml/" + i + "_treatments.sak").read().decode())
+                f.write(urllib.request.urlopen(
+                    "http://sakproject.ml/" + i + "_treatments.sak").read().decode())
                 f.close()
 
 
 def get_treatment_list() -> list:
-    file_list = os.listdir("../resources/treatments/")
+    file_list = os.listdir(sak_core.RESOURCE_DIR_PATH + "treatments/")
     treatment_list = []
     for fname in file_list:
-        f = open("../resources/treatments/" + fname, "r+")
+        f = open(sak_core.RESOURCE_DIR_PATH + "treatments/" + fname, "r+")
         treatment_list.append(json.loads(f.read())["name"])
     return treatment_list
--- treatment/treatment_gui.py
+++ treatment/treatment_gui.py
@@ -1,12 +1,18 @@
-from tkinter import *
 import treatment.treatment_core
+from tkinter import Tk, Label, Listbox
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 
 treatment_list = treatment.treatment_core.get_treatment_list()
 
 
 def start():
     window = Tk()
-    window.geometry(str(window.winfo_screenwidth()) + "x" + str(window.winfo_screenheight()))
+    window.geometry(str(window.winfo_screenwidth()) + "x" +
+                    str(window.winfo_screenheight()) + "+0+0")
     window.title('Treatment SAK')
     toptext = Label(window, text="Treatments List", width=window.winfo_screenwidth(),
                     height=window.winfo_screenheight() / 7)
@@ -15,26 +21,28 @@ def start():
     for i in range(len(treatment_list)):
         treatment_list_box.insert(i, treatment_list[i]["name"])
 
-    treatment_list_box.bind("<<ListboxSelect>>", callback)
+    treatment_list_box.bind("<<ListboxSelect>>", clicked)
     toptext.pack()
     treatment_list_box.pack()
     window.mainloop()
 
 
-def callback(event):
+def clicked(event):
     w = event.widget
     index = int(w.curselection()[0])
-    clicked(treatment_list[index])
+    treatment_info = treatment_list[index]
 
-
-def clicked(treatment):
     window = Tk()
-    window.geometry(str(window.winfo_screenwidth()) + "x" + str(window.winfo_screenheight()))
-    window.title('Treatment SAK - About ' + treatment["name"])
-    toptext = Label(window, text=treatment["name"], width=window.winfo_screenwidth(),
+    window.geometry(str(window.winfo_screenwidth()) + "x" +
+                    str(window.winfo_screenheight()) + "0+0")
+    window.title('Treatment SAK - About ' + treatment_info["name"])
+
+    toptext = Label(window, text=treatment_info["name"], width=window.winfo_screenwidth(),
                     height=window.winfo_screenheight() / 7)
-    doctext = Label(window, text=treatment["document"], justify="left", width=window.winfo_screenwidth(),
+    doctext = Label(window, text=treatment_info["document"], justify="left", width=window.winfo_screenwid(),
                     height=window.winfo_screenheight() * 6 / 7)
+
     toptext.pack()
     doctext.pack()
-    window.pack()
+
+    window.mainloop()
